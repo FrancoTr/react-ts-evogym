16c7d7a708a36dcb3b755f2364469b7df1c882f4
Franco Traverso <franco-t20@outlook.com>
2023-03-08 19:56:52 -0300

    Highlight current selected page on navbar

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -2,12 +2,20 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
 };
 
-function Link({ page }: Props) {
+function Link({ page, selectedPage, setSelectedPage }: Props) {
   const lowerCasePage = page.toLowerCase().replace(/ /g, "");
   return (
-    <AnchorLink className={} href={`#${lowerCasePage}`} onClick={}>
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
       {page}
     </AnchorLink>
   );
