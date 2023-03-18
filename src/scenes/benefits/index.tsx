ade461802d7b0036abfa2a652cfa403e6f176fac
Franco Traverso <franco-t20@outlook.com>
2023-03-17 23:27:37 -0300

    Move title tags into their own utility component

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,9 +1,11 @@
 import { SelectedPage } from "@/shared/types";
+import HText from "@/shared/HText";
 import {
   HomeModernIcon,
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -11,7 +13,15 @@ type Props = {
 
 function Benefits({ setSelectedPage }: Props) {
   return (
-    <section id="benefits" className="w-6/6 mx-auto min-h-full py-20"></section>
+    <section id="benefits" className="w-6/6 mx-auto min-h-full py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <div>
+          <HText>MORE THAN JUST A GYM.</HText>
+        </div>
+      </motion.div>
+    </section>
   );
 }
 
