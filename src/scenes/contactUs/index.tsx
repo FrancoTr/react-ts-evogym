879a7b431c380e060df18328b1b3936ed4445a97
Franco Traverso <franco-t20@outlook.com>
2023-03-25 22:50:25 -0300

    Add contact us form elements

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,7 +1,78 @@
-type Props = {};
+import { useForm } from "react-hook-form";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
 
-function ContactUs({}: Props) {
-  return <div>ContactUs</div>;
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+function ContactUs({ setSelectedPage }: Props) {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+            maiores enim architecto dolores porro quas consequuntur eos itaque.
+            Nulla corporis architecto incidunt quibusdam dolorum explicabo
+            dicta, possimus necessitatibus!
+          </p>
+        </motion.div>
+        {/* FORM AND IMAGE */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/a@email.com"
+              method="POST"
+            ></form>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
 }
 
 export default ContactUs;
