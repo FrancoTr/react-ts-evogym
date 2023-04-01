3a2f536f3b7f0d9d547e8d1dd52522253f93e034
Franco Traverso <franco-t20@outlook.com>
2023-03-31 21:34:46 -0300

    Add extra info in the footer section

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -13,8 +13,17 @@ const Footer = () => {
           </p>
           <p>© Evogym All Rights Reserved.</p>
         </div>
-        <div></div>
-        <div></div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Lorem, ipsum dolor.</p>
+          <p className="my-5">Lorem, ipsum dolor.</p>
+          <p>Lorem, ipsum.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Lorem, ipsum dolor.</p>
+          <p>(333)425-6825</p>
+        </div>
       </div>
     </footer>
   );
